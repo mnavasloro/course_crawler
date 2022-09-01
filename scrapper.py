387f407e3a2df45197eabd3ac11510ef2790551a
mnavas <mnavas@fi.upm.es>
2022-09-01 18:49:46 +0300

    csv files

--- scrapper.py
+++ scrapper.py
@@ -5,56 +5,77 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import urllib.request
 from urllib.parse import urlparse
+import csv
 
-url = "https://www.coursera.org/sitemap~www~courses.xml"
-response = urllib.request.urlopen(url)
-xml = BeautifulSoup(response, 
-                         'lxml-xml', 
-                         from_encoding=response.info().get_param('charset'))
-urls = xml.find_all("loc")
 
-df = pd.DataFrame(columns=['url', 'name', 'topics', 'skills', 'description'])
+dictionary = {'certificate': "https://www.coursera.org/sitemap~www~professional-certificate.xml", 
+              'specialization': "https://www.coursera.org/sitemap~www~onDemandSpecializations.xml", 
+              'course': "https://www.coursera.org/sitemap~www~courses.xml",
+              'mastertrack': "https://www.coursera.org/sitemap~www~mastertrack.xml", 
+              'project': "https://www.coursera.org/sitemap~www~guided-projects.xml"}
+# url = 
 
-x = len(urls)
-print(x)
-i=0
+for key in dictionary.keys():
+    
+  print(key + ' started!')
+  url = dictionary.get(key)
 
-for url_tag in urls:
-  i = i + 1
-  url = url_tag.text
+  response = urllib.request.urlopen(url)
+  xml = BeautifulSoup(response, 
+                          'lxml-xml', 
+                          from_encoding=response.info().get_param('charset'))
+  urls = xml.find_all("loc")
+  
+  f = open(key + 's.csv', 'w', encoding='UTF8', newline='')
+  w = csv.DictWriter(f, fieldnames=['url', 'name', 'topics', 'skills', 'type', 'description'], quoting=csv.QUOTE_ALL)
+  w.writeheader()
+  print(key + 's.csv created')
+  x = len(urls)
+  print(str(x) + ' items to parse:')
+  i=0
 
-  url_response = requests.get(url)
-  url_soup = BeautifulSoup(url_response.text, 'html.parser')
-  name = url_soup.title.string
+  for url_tag in urls:
+    i = i + 1
+    url = url_tag.text
 
-  try:
-    skills_tag = url_soup.find_all('span', {'class': '_ontdeqt'})
-    skills = []
-    for s in skills_tag:
-        skills.append(s.string)
+    url_response = requests.get(url)
+    url_soup = BeautifulSoup(url_response.text, 'html.parser')
 
-    description_tag = url_soup.find('div', {'class': 'description'})
-    description = description_tag.text
+    try:      
+      name = url_soup.title.string
+      skills_tag = url_soup.find_all('span', {'class': '_ontdeqt'})
+      skills = []
+      for s in skills_tag:
+          skills.append(s.string)
 
-    topics_tag = url_soup.find_all('a', {'data-track-component': 'breadcrumb_link'})
-    topics = []
-    for t in topics_tag:
-        topics.append(t.string)
-    topics.remove('Browse')
+      description_tag = url_soup.find('div', {'class': 'description'})
+      description = description_tag.text
+      description = description.replace('\n', '\\n').replace('\r', '\\r').encode('utf-8')
 
-    row = {
-                'url': url,
-                'name': name,
-                'topics' : topics,
-                'skills': skills,
-                'description': description,
-            }
+      topics_tag = url_soup.find_all('a', {'data-track-component': 'breadcrumb_link'})
+      topics = []
+      for t in topics_tag:
+          topics.append(t.string)
+      if 'Browse' in topics:
+        topics.remove('Browse')
 
-    df = df.append(row, ignore_index=True)
-    print(str(i) + '/' + str(x) + ': ' + url)
+      row = {
+                  'url': url,
+                  'name': name,
+                  'topics' : topics,
+                  'skills': skills,
+                  'type': key,
+                  'description': description,
+              }
 
-  except:
-    print('Problem while parsing ' + url)
+      w.writerow(row)
+      print(str(i) + '/' + str(x) + ': ' + url)
 
-df.to_csv('output_coursera.csv')
-print('Ended!')
+    except:
+      print('Problem while parsing ' + url)
+       
+  print(key + ' ended!')
+  f.close()
+  print('------------')
+
+print('Scrapping finished')
